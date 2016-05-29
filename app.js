a570ad591d9410b605cde3cd448c8459f48c0ca7
MirunaC# <miru_katzy@yahoo.com>
2016-05-30 01:48:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -83,31 +83,19 @@ app.listen(port);
 const express = require('express');
 const aws = require('aws-sdk');
 
-/*
- * Set-up and run the Express app.
- */
+
 const app = express();
 app.set('views', './views');
 app.use(express.static('./public'));
 app.engine('html', require('ejs').renderFile);
 app.listen(process.env.PORT || 3000);
 
-/*
- * Load the S3 information from the environment variables.
- */
+
 const S3_BUCKET = "dmg0";
 
-/*
- * Respond to GET requests to /account.
- * Upon request, render the 'account.html' web page in views/ directory.
- */
-app.get('/account', (req, res) => res.render('/views/account.html'));
 
-/*
- * Respond to GET requests to /sign-s3.
- * Upon request, return JSON containing the temporarily-signed S3 request and
- * the anticipated URL of the image.
- */
+app.get('/account', (req, res) => res.render('./views/account.html'));
+
 app.get('/sign-s3', (req, res) => {
   const s3 = new aws.S3();
   
@@ -140,11 +128,6 @@ app.get('/sign-s3', (req, res) => {
   });
 });
 
-/*
- * Respond to POST requests to /submit_form.
- * This function needs to be completed to handle the information in
- * a way that suits your application.
- */
 app.post('/save-details', (req, res) => {
  res.send('ok');
 });
