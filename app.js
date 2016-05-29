91eb08ada69139a10b786beb4a575897689791bb
MirunaC# <miru_katzy@yahoo.com>
2016-05-30 02:30:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -123,6 +123,7 @@ app.get('/sign-s3', (req, res) => {
     const returnData = {
       signedRequest: data,
       url: `https://s3.amazonaws.com/${S3_BUCKET}/${fileName}`
+      console.log(url);
     };
     res.write(JSON.stringify(returnData));
     res.end();
